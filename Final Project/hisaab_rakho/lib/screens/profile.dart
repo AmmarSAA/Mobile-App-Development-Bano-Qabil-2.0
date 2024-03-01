442259a8b04a4abd4dc2d7dbb5ab3aa6d107472e
Syed Ammar Ahmed <s.ammarahmed14@gmail.com>
2024-03-02 04:15:46 +0500

    FEAT: Basic Template for Profile Screen

--- Final Project/hisaab_rakho/lib/screens/profile.dart
+++ Final Project/hisaab_rakho/lib/screens/profile.dart
@@ -5,12 +5,56 @@
 **************************/
 
 import 'package:flutter/material.dart';
+import './dashboard.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        
+      ),
+
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+          ),
+        ],
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Dashboard()),
+              );
+              break;
+            case 1:
+              // Handle Add button tap
+              break;
+            case 2:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Profile()),
+              );
+              break;
+          }
+        },
+      ),
+    );
   }
 }
